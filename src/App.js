ab3f82215f3aa5644902d386f9c6bf007f7d6403
Mary Franks <maryfranks@gmail.com>
2018-01-17 09:30:30 -0500

    move FilmDetails and FilmListing to own components

--- src/App.js
+++ src/App.js
@@ -1,18 +1,15 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import FilmListing from './FilmListing';
+import FilmDetails from './FilmDetails';
 
 class App extends Component {
   render() {
     return (
       <div className="film-library">
-        <div className="film-list">
-          <h1 className="section-title">FILMS</h1>
-        </div>
-
-        <div className="film-details">
-          <h1 className="section-title">DETAILS</h1>
-        </div>
+        <FilmListing />
+        <FilmDetails />
       </div>
     );
   }
