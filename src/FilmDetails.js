bc29053741e75f76f95a97423541d50aac53eb49
Mary Franks <maryfranks@gmail.com>
2018-01-19 11:32:43 -0500

    refactor FilmDetails into functional component

--- src/FilmDetails.js
+++ src/FilmDetails.js
@@ -1,13 +1,11 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class FilmDetails extends Component {
-  render() {
+function FilmDetails(props) {
     return (
       <div className="film-details">
         <h1 className="section-title">DETAILS</h1>
       </div>
     );
-  }
 }
 
 export default FilmDetails;
